2205dc8cad11afe375730246038ee1ecd222d6a1
Nikolay_Cholakov <nikolaicholakov25@gmail.com>
2023-04-11 19:41:46 +0300

    Project set up

---
+++ src/components/CsvInputForm/CsvInputForm.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent, useRef } from "react";
+
+interface CsvInputFormProps {}
+
+export const CsvInputForm: FunctionComponent<CsvInputFormProps> = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  return <div></div>;
+};
